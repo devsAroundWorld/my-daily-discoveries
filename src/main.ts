9ed6b428e235809a712de5a03c368672ae3a1e00
LuisDev2001 <luisqrja@gmail.com>
2023-09-21 22:10:34 -0500

    feat: MDD-39 - Install and configure FormKit w/ theme genesis

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import '@/assets/styles/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { plugin, defaultConfig } from '@formkit/vue'
+import '@formkit/themes/genesis'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(plugin, defaultConfig({
+  theme: 'genesis'
+}))
 app.mount('#app')
