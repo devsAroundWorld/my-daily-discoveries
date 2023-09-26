60cb09ca1e32aba4c80d020f17a4ca1f185d250c
andrea7896 <andrea_nataly_05@hotmail.com>
2023-09-25 18:26:32 -0600

    modify indentation of code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
-
-/* import the fontawesome core */
-  import { library } from '@fortawesome/fontawesome-svg-core'
-  import { faStar} from '@fortawesome/free-regular-svg-icons'
-
-  library.add(faStar)
-
 </script>
 
 <template>
   <main>
-    Home View <font-awesome-icon icon="fa-solid fa-star" /><font-awesome-icon icon="fa-regular fa-star" />
+    Home View
   </main>
 </template>
