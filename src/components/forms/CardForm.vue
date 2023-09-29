ca02057ebfc5640603b436f789192dfa0c32d19b
LuisDev2001 <luisqrja@gmail.com>
2023-09-28 23:01:05 -0500

    feat: mdd-50 - Clean App.vue and fix color for card form

--- src/components/forms/CardForm.vue
+++ src/components/forms/CardForm.vue
@@ -11,10 +11,11 @@
   border-radius: .5rem;
   overflow: hidden;
   padding: .5rem 1rem;
-  background: rgba(229, 217, 242, 0.34);
+  background: rgba(255, 255, 255, 0.51);
   border-radius: 16px;
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
 }
 </style>
