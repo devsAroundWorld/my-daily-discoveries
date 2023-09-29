a46ee447d6fcf85f877fcf850622292e1fa50f74
LuisDev2001 <luisqrja@gmail.com>
2023-09-28 22:57:14 -0500

    feat: MDD-50 - Create component for BgFormPrincipal and CardForm

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
   import { RouterView } from 'vue-router'
+  import BgFormPrincipal from './components/forms/BgFormPrincipal.vue'
+  import CardForm from './components/forms/CardForm.vue'
 </script>
 
 <template>
   <RouterView />
+  <BgFormPrincipal>
+    <CardForm>
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+      Hola soy una card de un form
+    </CardForm>
+  </BgFormPrincipal>
 </template>
---
+++ src/components/forms/CardForm.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card-form">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card-form {
+  border-radius: .5rem;
+  overflow: hidden;
+  padding: .5rem 1rem;
+  background: rgba(229, 217, 242, 0.34);
+  border-radius: 16px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+}
+</style>
