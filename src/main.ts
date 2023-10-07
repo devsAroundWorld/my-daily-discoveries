e0d7a38adec197de65db7daa6c130c9a920746d3
LuisDev2001 <luisqrja@gmail.com>
2023-10-07 11:26:12 -0500

    feat: MDD-1 - Config observe session and navigation guards

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { firebaseApp } from '@/plugins/firebase.config'
+import '@/plugins/firebase.config'
 import { plugin } from '@formkit/vue'
 import formKitConfig from '@/plugins/formkit.config'
 import App from './App.vue'
@@ -10,7 +10,9 @@ import '@/assets/styles/index.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+
+app.use(pinia)
 
 app.use(router)
 
@@ -21,5 +23,4 @@ app.component('FontAwesomeIcon', FontAwesomeIcon)
 app.mount('#app')
 
 // TODO: Solo apra contrastar de que estamos conectados se dejará el console, al final ELIMINARLO!
-console.log({firebaseApp})
-console.log(import.meta.env.MODE)
+console.log(`Estas en modo: ${import.meta.env.MODE}`)
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+import type { UserDataInterface } from '@/models/User'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue'),
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/sign-up',
+    name: 'sign-up',
+    component: () => import('../views/SignUpView.vue'),
+    meta: {
+      requiresAuth: false
+    }
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/DashboardView.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    },
-    {
-      path: '/sign-up',
-      name: 'sign-up',
-      component: () => import('../views/SignUpView.vue')
-    },
-  ]
+  routes,
+})
+
+router.beforeEach(async (to, from, next) => {
+  const store = useAuthStore()
+  const user = await store.currentUser() as UserDataInterface
+  if (to.meta.requiresAuth && !user?.uid  && !store.isLoggedIn) {
+    //TODO: Mandar un toast notifucation de necesitas autenticarte
+    console.log('Necesitas auntenticarte')
+    next({ name: 'home' })
+  } else {
+    next()
+  }
 })
 
 export default router
