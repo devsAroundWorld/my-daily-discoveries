4d61ed871397273c91203e74f638d2ebc8001a10
LuisDev2001 <luisqrja@gmail.com>
2023-10-09 22:11:39 -0500

    fix: MDD-47 - Delete files unnecesary

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-  import ResetPassword from '../components/ResetPassword/ResetPassword.vue'
 </script>
 
 <template>
   <main>
     Home View
-    <!-- <ResetPassword /> -->
   </main>
 </template>
