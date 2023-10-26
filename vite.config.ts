fd17bba81aea6850e994e2a6e1e7f5f61365518b
LuisDev2001 <luisqrja@gmail.com>
2023-10-25 22:37:59 -0500

    fix: Cache files js tryit one

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import manifestCustom from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     VitePWA({
@@ -22,5 +23,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
 })
