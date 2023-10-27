5b7867a1ee199014dc2b0ee6e7382ed85b88a391
LuisDev2001 <luisqrja@gmail.com>
2023-10-26 21:54:15 -0500

    feaT: MDD-23 - Add Notification composable to use

--- src/views/private/DashboardView.vue
+++ src/views/private/DashboardView.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts">
+  import { onMounted, onBeforeUnmount, ref } from 'vue'
   import MotivationalPhrases from '@/components/dashboard/MotivationalPhrases.vue'
   import RecognitionMedals from '@/components/dashboard/RecognitionMedals.vue'
+  import { useDashboardStore } from '@/stores/dashboard'
+  import useNotification from '@/composables/useNotification'
+
+  const dashboardStore = useDashboardStore()
+
+  const { showNotification } = useNotification()
+
+  const notificationInterval = ref(0)
+
+  const showPeriodicNotification = () => {
+    showNotification('My Daily Discoveries - La Frase Del Día Es:', dashboardStore.phrase)
+  }
+
+  onMounted(() => {
+    showPeriodicNotification()
+    notificationInterval.value = setInterval(showPeriodicNotification, 3600000)
+  })
+
+  onBeforeUnmount(() => {
+    clearInterval(notificationInterval.value)
+  })
 </script>
 
 <template>
